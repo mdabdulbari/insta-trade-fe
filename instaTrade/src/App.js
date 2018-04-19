ca84a19521a19a85242e4e8dcf7d6654fd42a7e8
INmabari <inmabari@INmabari.local>
2018-04-19 11:10:56 +0530

    Add connection server-client

--- instaTrade/src/App.js
+++ instaTrade/src/App.js
@@ -11,10 +11,11 @@ import Login from './Login/Login';
 import Register from './Register/Register';
 
 class App extends Component {
+  
   render() {
     return (
       <BrowserRouter>
-      <div>        
+      <div>
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/dashboard" component={Dashboard} exact />
--- instaTrade/src/Dashboard/Dashboard.js
+++ instaTrade/src/Dashboard/Dashboard.js
@@ -1,16 +1,49 @@
 import React from "react";
 import "./Dashboard.css";
 import Header from "../Header/Header";
+import Loading from "../common/Loading";
 
-const Dashboard = () => {
-    return(
-        <div>
-        <Header />
-            <div className="topmargin">
-            This is dashboard
+class Dashboard extends React.Component{
+
+    constructor(){
+        super();
+        this.state = {
+            loading: true,
+            posts: ''
+        }
+    }
+
+    componentWillMount(){
+        this.setState({ loading: true });
+        this.callApi()
+            .then(content => {
+                this.setState({ posts: content, loading: false})
+                console.log("content received");
+            })
+    }
+    
+    async callApi(){
+        const response = await fetch('http://localhost:8080/dashboard');
+        return await response.json();
+    }
+
+    render(){
+        if (this.state.loading){
+            return(
+                <div className="loading-div">
+                    <Loading />
+                </div>
+            );
+        }
+        return(
+            <div>
+            <Header />
+                <div className="topmargin">
+                    {this.state.posts[0].content}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Dashboard;
